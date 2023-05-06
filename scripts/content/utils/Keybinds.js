dc04812a673759c1d5e807d43fcd261c9c8f632f
Lukas Olsen <61273323+lukasolsen@users.noreply.github.com>
2023-05-06 22:18:39 +0200

    Fixed keybinds as it now registers, however backend not working.

--- scripts/content/utils/Keybinds.js
+++ scripts/content/utils/Keybinds.js
@@ -3,52 +3,59 @@ let isLoading = true;
 // TODO: Fix this event, currently does not work. Just records the keys.
 // ! Not working, creates a problem at start.
 const listenForKeybinds = (keybind, action) => {
-  const keys = keybind.split("+");
+    const keys = keybind.split("+");
 
-  const keyDownListener = (event) => {
-    if (isLoading) {
-      return;
+    const keyDownListener = (event) => {
+        if (isLoading) {
+            return;
+        }
+
+        const pressedKeys = keys.map((key) => {
+            let cusKey = key.toLowerCase();
+            switch (cusKey) {
+                case "ctrl":
+                    return event.ctrlKey;
+                case "shift":
+                    return event.shiftKey;
+                case "alt":
+                    return event.altKey;
+                default:
+                    return event.key.toUpperCase() === key.toUpperCase();
+            }
+        });
+
+        if (pressedKeys.every((isPressed) => isPressed)) {
+            if (action) {
+                PREDEFINED_ACTIONS[action];
+            }
+        }
+    };
+
+    // Add the listener when the flag is true
+    if (!isLoading) {
+        document.addEventListener("keydown", keyDownListener);
     }
 
-    const pressedKeys = keys.map((key) => {
-      let cusKey = key.toLowerCase();
-      switch (cusKey) {
-        case "ctrl":
-          return event.ctrlKey;
-        case "shift":
-          return event.shiftKey;
-        case "alt":
-          return event.altKey;
-        default:
-          return event.key.toUpperCase() === key.toUpperCase();
-      }
-    });
-
-    if (pressedKeys.every((isPressed) => isPressed)) {
-      if (action) {
-        console.log("Action works.", action);
-        PREDEFINED_ACTIONS[action];
-      }
-    }
-  };
-
-  // Add the listener when the flag is true
-  if (!isLoading) {
-    document.addEventListener("keydown", keyDownListener);
-  }
-
-  return {
-    enable: () => {
-      isLoading = false;
-      document.addEventListener("keydown", keyDownListener);
-    },
-    disable: () => {
-      isLoading = true;
-      document.removeEventListener("keydown", keyDownListener);
-    },
-  };
+    return {
+        enable: () => {
+            isLoading = false;
+            document.addEventListener("keydown", keyDownListener);
+        },
+        disable: () => {
+            isLoading = true;
+            document.removeEventListener("keydown", keyDownListener);
+        },
+    };
 };
 
 const PREDEFINED_ACTIONS = {
-  "extension.toggle": sendMessageBackend("toggleExtension"),
+    "extension.toggle": sendMessageBackend("toggleExtension"),
 };
+
+// Disable the listener by default
+listenForKeybinds("ctrl+shift+e", "extension.toggle").disable();
+
+// When the extension is ready, enable the listener
+window.addEventListener("load", () => {
+    listenForKeybinds("ctrl+shift+e", "extension.toggle").enable();
+});
